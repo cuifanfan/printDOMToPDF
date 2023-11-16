c63f1319dd9813ca8c4d3f32cc998d296ce9e38d
cuifan <cuifan@isv-tech.com>
2023-11-16 14:50:38 +0800

    修改范围：初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,163 @@
-# copier
+# 写在前面
+
+在前端开发中，HTML转PDF是常见需求，这一块儿有两个常见的插件：html2canvas和jspdf，但这种纯前端打印的方式有一些缺点：
+
+1. PDF清晰度不高，模糊
+2. 多页PDF会出现内容分割
+3. 页面较宽或较长时，PDF打印内容不全
+
+因此，笔者经过一些调研之后，决定采用无头浏览器的方式生成PDF，最终代码运行在BFF层，公司内部其他系统也可以接入。
+
+无头浏览器学习地址：
+
+- [Puppeteer 中文网 (nodejs.cn)](https://pptr.nodejs.cn/)
+- [Puppeteer 简介 | Puppeteer 中文文档 | Puppeteer 中文网 (bootcss.com)](https://puppeteer.bootcss.com/)
+
+# 项目代码
+
+https://github.com/cuifanfan/pdf_html_node
+
+# 环境依赖：
+
+## Node版本
+
+```js
+nvm use 18.9.0
+```
+
+## 打包工具
+
+```shell
+npm i -g pkg@5.8.1
+```
+
+## 安装依赖
+
+```shell
+npm i
+```
+
+# 打包
+
+## `win`平台：
+
+```shell
+pkg .\pdfgenerator.js -t win -o pdfgenerator
+```
+
+# 配置文件说明
+
+## puppeteer.config.cjs
+
+更改 `Puppeteer` 用于安装浏览器的默认缓存目录。
+
+## pdfgenerator.config.json
+
+服务配置信息，如端口、日志大小限制等
+
+## .cache
+
+`Puppeteer`使用的无头浏览器缓存目录
+
+## pdfgenerator.vbs
+
+隐藏`pdfgenerator.exe`服务窗口
+
+# 导出多页HTML字符串为PDF
+
+## 接口功能描述
+
+## 请求内容
+
+| 请求方法 | 请求URI      | URI示例                      |
+| -------- | ------------ | ---------------------------- |
+| POST     | /point_graph | /v1/pdfgenerator/point_graph |
+
+## 请求参数
+
+| 字段         | 类型   | 是否必传 | 默认值    | 备注                     |
+| ------------ | ------ | -------- | --------- | ------------------------ |
+| htmlContents | Array  | 否       | []        | 需要打印的HTML字符串数组 |
+| width        | Number | 否       | 485       | 输出PDF每页的宽度        |
+| height       | Number | 否       | 275       | 输出PDF每页的高度        |
+| host         | String | 否       | /v1/file/ | 第三方静态资源主机地址   |
+
+## 返回内容
+
+> responseType为Arraybuffer.
+
+```js
+Response {
+    body:ReadableStream,
+    bodyUsed:true,
+    headers:Headers{},
+    ok:true,
+    redirected:false,
+    statusText:'OK',
+    type:'cors',
+    url:'http://127.0.0.1:16695/v1/pdfgenerator/point_graph'
+}
+```
+
+前端代码示例：
+
+```js
+/**
+ * 
+ * @param {Object} 配置参数
+ *        @param url: 后台服务地址
+ *        @param data: {
+                    htmlContents: html字符串组成的数组
+                    host: 第三方静态资源地址（比如图片、CSS等其他url）
+                    width: 导出PDF每页的宽
+                    height: 导出PDF每页的高
+                  }
+ *        @param fileName: pdf的名称
+ */
+async function printPage({ url, data, fileName }) {
+  const response = await fetch(url, {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    responseType: 'arraybuffer'
+  });
+  // 转为 buffer
+  const buffer = await response.arrayBuffer();
+  const _a = document.createElement('a');
+  const URL = window.URL || window.webkitURL;
+  _a.href = URL.createObjectURL(new Blob([buffer], { type: 'arraybuffer' }));
+  _a.download = fileName + '.pdf';
+  document.body.appendChild(_a);
+  _a.click();
+  document.body.removeChild(_a);
+  window.URL.revokeObjectURL(_a.href);
+}
+
+// 填充内容并导出整个页面中某些元素
+const page1 = document.getElementById('page1');
+const table1 = ['time', 'product', 'station', 'color', 'VIN'];
+const table2 = ['defects', 'hood', 'trunk_Lid', 'fender_L', 'fender_R', 'front_Door_L', 'front_Door_R', 'rear_Door_L', 'rear_Door_R', 'side_Panel_L', 'side_Panel_R', 'roof'];
+(async () => {
+  const count = 1;
+  const url = 'http://127.0.0.1:16695/v1/pdfgenerator/point_graph';
+  const fileName = 'cuifanfan' + Math.random();
+  const data = {
+    htmlContents: [],
+    host: 'http://172.17.17.38:8081/Desktop/htmltemplate/',
+    width: page1.clientWidth + 190,
+  }
+  for (let i = 0; i < count; i++) {
+    const ids = [...table1, ...table2];
+    for (const id of ids) {
+      const el = document.getElementById(id);
+      el.innerText = id + i;
+    }
+    data.htmlContents = [document.documentElement.outerHTML];
+    await printPage({ url, fileName, data })
+  }
+})()
+```
+>>>>>>> a8f3ebc (修改范围：Node无头浏览器PDF导出服务)
 
---
+++ pdfgenerator.js
@@ -0,0 +1,193 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
+const log4js = require('log4js');
+const { PDFDocument } = require('pdf-lib');
+const expressArtTemplate = require('express-art-template');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+// 获取日志logger对象
+const logger = log4js.getLogger();
+log4js.configure({
+    replaceConsole: true,
+    appenders: {
+        cheese: {
+            // 设置类型为 dateFile
+            type: 'dateFile',
+            // 配置文件名为 myLog.log
+            filename: 'logs/pdfgenerator.log',
+            // 指定编码格式为 utf-8
+            encoding: 'utf-8',
+            // 配置 layout，此处使用自定义模式 pattern
+            layout: {
+                type: 'pattern',
+                // 配置模式
+                // pattern: '{"date":"%d","level":"%p","category":"%c","host":"%h","pid":"%z","data":\'%m\'}'
+                pattern: '%d %p %m'
+            },
+            // 日志文件按日期（天）切割
+            pattern: 'yyyy-MM-dd',
+            // 回滚旧的日志文件时，保证以 .log 结尾 （只有在 alwaysIncludePattern 为 false 生效）
+            keepFileExt: true,
+            // 输出的日志文件名是都始终包含 pattern 日期结尾
+            alwaysIncludePattern: true
+        },
+    },
+    categories: {
+        // 设置默认的 categories
+        default: { appenders: ['cheese'], level: 'debug' },
+    }
+});
+
+// 读取自定义配置信息
+const config = JSON.parse(fs.readFileSync('./pdfgenerator.config.json'));
+const port = config.port;
+const logLimitSize = config.logs.size;
+const viewsPath = config.viewsPath;
+
+const app = express();
+
+// 解析POST请求参数
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+// 处理跨域
+app.use(cors());
+
+// 配置模板引擎
+app.engine('html', expressArtTemplate);
+app.set('views', viewsPath);
+app.set('view engine', 'html');
+const cssTemplate = fs.readFileSync(path.join(viewsPath, './reportTemplate.css'));
+/**
+ * @description 获取文件夹大小
+ * @param {String} folderPath 文件夹路径
+ * @param {Function} errHandler 错误处理函数
+ * @returns
+ */
+function getFolderSize(folderPath, errHandler) {
+    try {
+        const fileNames = fs.readdirSync(folderPath);
+        let totalSize = 0;
+        fileNames.forEach(fileName => {
+            const filePath = path.join(folderPath, fileName);
+            totalSize += fs.statSync(filePath).size;
+        });
+        return totalSize;
+    } catch (err) {
+        errHandler && errHandler(err);
+    }
+}
+
+/**
+ * @description 删除文件夹下所有文件
+ * @param {String} folderPath 文件夹路径
+ * @param {Function} errHandler 错误处理函数
+ */
+function clearHalfFolder(folderPath, errHandler) {
+    try {
+        const fileNames = fs.readdirSync(folderPath);
+        // 每次删除一半
+        const halfFileNames = fileNames.slice(0, Math.floor(fileNames.length / 2));
+        halfFileNames.forEach(fileName => {
+            const filePath = path.join(folderPath, fileName);
+            fs.unlinkSync(filePath);
+        });
+
+    } catch (err) {
+        errHandler && errHandler(err);
+    }
+}
+
+/**
+ * @description 合并多个pdf为一页
+ * @param {Array} pdfs
+ * @returns {Buffer} pdf buffer数据
+ */
+async function mergePDF(pdfs) {
+    // 创建新的 PDF 文档
+    const pdfDocs = await PDFDocument.create();
+    for (const pdf of pdfs) {
+        // 读取当前PDF
+        const pdfItem = await PDFDocument.load(pdf);
+        // 复制每一页
+        const pageCount = pdfItem.getPageCount();
+        for (let i = 0; i < pageCount; i++) {
+            const [PDFPageItem] = await pdfDocs.copyPages(pdfItem, [i]);
+            pdfDocs.addPage(PDFPageItem);
+        }
+    }
+    return pdfDocs.save();
+}
+
+/**
+ * @description 处理引用地址
+ * @param {String} html html文本
+ * @param {String} host 引用的根地址
+ * @returns {String} 处理后的URL
+ */
+function handleHTMLReference(html, host) {
+    return html.replace(/(src|href)="((?!data:.*?;base64,).*?)"/g, `$1="${host}/$2"`)
+               .replace('/***ISV_PDF_STYLE***/', cssTemplate);
+}
+
+app.post('/v1/pdfgenerator/get_template', (request, response) => {
+    const data = request.body;
+    data.pageNum = 2 + Math.ceil((data.Pictures.length - 7) / 11); // PDF总页数
+    response.render('reportTemplate.html', data);
+})
+
+app.post('/v1/pdfgenerator/point_graph', (request, response) => {
+    // html列表、文件名、PDF宽高、第三方引用地址、缩放倍数
+    let { htmlContents, width, height, host, scale } = request.body;
+    width = width ? Number(width) : 1200;
+    height = height ? Number(height) : 1200;
+    host = host ? host : 'http://127.0.0.1:' + port;
+    scale = scale ? Number(scale) : 1;
+    scale = Math.min(Number(scale), 2);
+    // 处理第三方资源引用
+    for (let i = 0; i < htmlContents.length; i++) {
+        htmlContents[i] = handleHTMLReference(htmlContents[i], host);
+    }
+    // 日志路径
+    const logPath = './logs';
+    const logSize = getFolderSize(logPath, (err) => logger.error(err));
+    // 如果日志文件夹大小大于1M，删除文件夹下一半文件
+    if (logSize > logLimitSize) clearHalfFolder(logPath, (err) => logger.error(err));
+
+    (async () => {
+        try {
+            const browser = await puppeteer.launch({
+                headless: 'new',
+            });
+            const pdfs = [];
+            const page = await browser.newPage();
+            for (const htmlContent of htmlContents) {
+                // 为每个html单独创建一个页面并打印为PDF
+                await page.setContent(htmlContent);
+                pdfs.push(
+                    await page.pdf({
+                        width: width,
+                        height: height,
+                        scale: scale,
+                        margin: { top: '25px', left: '10px', right: '10px' },
+                        printBackground: true, // 保留背景
+                        '-webkit-print-color-adjust': 'exact'
+                    })
+                );
+            }
+            // 合并所有页面的PDF，得到Buffer，并压缩
+            const pdfBuffer = Buffer.from(await mergePDF(pdfs));
+            response.setHeader('Content-type', 'application/pdf');
+            response.send(pdfBuffer);
+            await browser.close();
+        } catch (err) {
+            logger.error(err);
+        }
+    })();
+});
+
+app.listen(port, () => {
+    console.log(`welcome, server is running at ${port}...`);
+});
